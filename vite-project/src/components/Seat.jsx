efde4b5784b2bc0db1fde3663ad80133c32aace2
Ariel Monroy <ariel.monroy@jala.university>
2025-06-04 16:00:10 -0400

    fix: correct seat availability toggle logic and button click handler

--- vite-project/src/components/Seat.jsx
+++ vite-project/src/components/Seat.jsx
@@ -4,13 +4,13 @@ export default function Seat({ number, row, onClick }) {
   const [available, setIsAvailable] = useState(true)
 
   const handleClick = () => {
-    setIsAvailable((prev) => { !prev })
+    setIsAvailable((prev) => { return !prev })
     onClick(row, number, available)
   }
 
   return (
-    <div className={`seat ${available ? "seat-available" : "seat-ocuped"}`}>
-      <button className="seat" onClick={() => handleClick}>
+    <div className={`seat ${available ? "seat-available" : "seat-ocupped"}`}>
+      <button onClick={handleClick}>
         Seat
       </button>
     </div>
