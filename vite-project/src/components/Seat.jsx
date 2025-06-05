61be7317057e2af43a501ff315cadfbe268a71a6
Ariel Monroy <ariel.monroy@jala.university>
2025-06-04 22:29:14 -0400

    fix: update Seat component logic and styles for better user experience

--- vite-project/src/components/Seat.jsx
+++ vite-project/src/components/Seat.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react"
 import "./Seat.css"
-export default function Seat({ number, row, onClick }) {
+export default function Seat({ number, row, onBookSeat }) {
   const [available, setIsAvailable] = useState(true)
 
   const handleClick = () => {
-    setIsAvailable((prev) => { return !prev })
-    onClick(row, number, available)
+    if (available) {
+      setIsAvailable(false)
+      onBookSeat()
+      alert(`You booked seat  ${String.fromCharCode(row + 65)}-${number + 1}`)
+    } else {
+      alert(`Seat ${String.fromCharCode(row + 65)}-${number + 1} is already booked`)
+    }
   }
 
   return (
-    <div className={`seat ${available ? "seat-available" : "seat-ocupped"}`}>
-      <button onClick={handleClick}>
-        Seat
-      </button>
-    </div>
+
+    <button className={`seat ${available ? "seat-available" : "seat-occupied"}`} onClick={handleClick}>
+      {String.fromCharCode(row + 65)}-{number + 1}
+    </button>
+
   )
 }
