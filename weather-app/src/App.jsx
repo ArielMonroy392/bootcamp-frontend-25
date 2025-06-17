71d1bc562c1a136434fc1d2555dd2a48b32e1d1a
Ariel Monroy <ariel.monroy@jala.university>
2025-06-17 16:32:52 -0400

    refactor: simplify App component

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,52 +1,9 @@
-import { useRef, useState } from 'react'
 import './App.css'
-import Input from './components/atoms/input'
-import WeatherInfo from './components/molecules/WeatherInfo'
+import Weather from './components/organisms/Weather'
 
 function App() {
-  const inputRef = useRef(null)
-  const [history, setHistory] = useState({})
-  const [currentWeather, setCurrentWeather] = useState({})
-  const mockWeatherData = {
-    'New York': {
-      temperature: '22°C',
-      humidity: '56%',
-      windSpeed: '15 km/h'
-    },
-    'Los Angeles': {
-      temperature: '27°C',
-      humidity: '45%',
-      windSpeed: '10 km/h',
-    },
-    'London': {
-      temperature: '15°C',
-      humidity: '70%',
-      windSpeed: '20 km/h'
-    },
-  };
-
-  const onSearch = () => {
-    const city = inputRef.current.value
-    const weather = mockWeatherData[city]
-    if (!weather) {
-      alert("We dont know that city 😰")
-    } else {
-
-      setHistory((prev) => {
-        prev[city] = weather
-        return { ...prev }
-      })
-      setCurrentWeather(weather)
-      console.log(weather)
-      console.log(history)
-    }
-  }
   return (
-    <>
-      <Input ref={inputRef} placeholder={"Select your city"} />
-      <button onClick={onSearch}>Search</button>
-      <WeatherInfo city={currentWeather} />
-    </>
+    <Weather></Weather>
   )
 }
 
