93a82371975b5adb2c0eb5aad038a1ddd77367fb
Ariel Monroy <ariel.monroy@jala.university>
2025-06-17 16:26:42 -0400

    fix: remove unnecessary justifyContent style from WeatherInfo component

--- weather-app/src/components/molecules/WeatherInfo.jsx
+++ weather-app/src/components/molecules/WeatherInfo.jsx
@@ -1,6 +1,6 @@
 export default function WeatherInfo({ city }) {
   return (
-    <div style={{ display: "flex", flexDirection: "column", justifyContent: "start" }}>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <span>Temperature: {city.temperature}</span>
       <span>Humidity: {city.humidity}</span>
       <span>Wind Speed: {city.windSpeed}</span>
