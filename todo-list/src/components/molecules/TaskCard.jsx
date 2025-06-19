239624e4c8c117b4909c1fc504de6afb7ef58bab
Ariel Monroy <ariel.monroy@jala.university>
2025-06-19 17:01:03 -0400

    feat: update TaskCard component layout and functionality with improved styling and state management

--- todo-list/src/components/molecules/TaskCard.jsx
+++ todo-list/src/components/molecules/TaskCard.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import Button from "../atoms/Button";
 import CheckBox from "../atoms/CheckBox";
 import "./TaskCard.css"
 
-export default function TaskCard  ({ task, onEdit, onDelete }) {
+export default function TaskCard({ task, onEdit, onDelete }) {
+  const [isDone, setIsDone] = useState(false)
 
-  
   return (
     <div className="card" >
-      <CheckBox ></CheckBox>
-      <span>{task.name}</span>
-      <Button onClick={() => { onEdit(task)}}>Edit</Button>
-      <Button onClick={() => { onDelete(task)}}>Delete</Button>
+      <CheckBox value={isDone} onChange={setIsDone}></CheckBox>
+      <span style={{textDecoration: isDone ? "line-through" : ''}}>{task.name}</span>
+      {
+        !isDone && (<>
+          <Button onClick={() => { onEdit(task) }}>Edit</Button>
+          <Button onClick={() => { onDelete(task) }}>Delete</Button></>)
+      }
     </div>
   )
 }
