03d96c0182dcc7af1cff175213d612f09fe35120
Ariel Monroy <ariel.monroy@jala.university>
2025-06-20 17:05:10 -0400

    feat: refactor App component to use useReducer for task management and improve state handling

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,32 +1,56 @@
-import { useState, useRef } from 'react';
+import {  useRef, useReducer } from 'react';
 import './App.css';
 import Input from './components/atoms/Input';
 import Button from './components/atoms/Button';
 import TaskCard from './components/molecules/TaskCard';
 
+function tasksReducer(tasks, action) {
+  if (action.type === 'add') {
+    return [...tasks, { id: action.id, name: action.name, isEditing: action.isEditing, isCompleted: action.isCompleted  }]
+  }
+  if (action.type === 'edit') {
+    console.log(action)
+    return tasks.map((t) => (t.id === action.task.id ? action.task : t))
+  }
+  if (action.type === 'delete') {
+    return tasks.filter((t) => t.id !== action.id)
+  }
+  if (action.type === 'complete') {
+    return tasks.map((t) => {
+      if (t.id === action.id) t.isCompleted = action.value
+      return t
+    })
+  }
+  if (action.type === 'setEditing') {
+    return tasks.map((t) => {
+      if (t.id === action.id) t.isEditing = true
+      return t
+    })
+  }
+  throw new Error("Unknown type")
+}
+
+
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [selectedTask, setSelectedTask] = useState(null);
+  const [tasks, dispatch] = useReducer(tasksReducer, [])
+  // const [selectedTask, setSelectedTask] = useState(null);
   const inputRef = useRef(null);
 
-  const isValidTask = (value) => {
-    return value !== "";
-  };
-
   const addTask = (name) => {
-    setTasks((prev) => [...prev, { id: Math.random(), name }]);
+    dispatch({
+      type: 'add',
+      id: Math.random(),
+      name: name,
+      isCompleted: false,
+      isEditing: false,
+    })
   };
 
-  const editTask = (name) => {
-    const updatedTask = { id: selectedTask.id, name };
-    setTasks((prev) =>
-      prev.map((task) => (task.id === selectedTask.id ? updatedTask : task))
-    );
-    setSelectedTask(null);
-  };
-
-  const clearInput = () => {
-    if (inputRef.current) inputRef.current.value = "";
+  const editTask = (task) => {
+    dispatch({
+      type: 'edit',
+      task: task
+    })
   };
 
   const submitTask = () => {
@@ -35,55 +59,63 @@ function App() {
       alert("Not valid task");
       return;
     }
-
-    if (selectedTask) {
-      editTask(value);
+    const editingTask = tasks.find((t) => t.isEditing)
+    console.log(editingTask)
+    if (editingTask) {
+      editTask({ ...editingTask, name: value, isEditing: false });
     } else {
       addTask(value);
     }
-
     clearInput();
   };
 
   const handleEditTask = (task) => {
-    setSelectedTask(task);
+    dispatch({type: "setEditing", id: task.id})
     if (inputRef.current) inputRef.current.value = task.name;
   };
 
   const handleDeleteTask = (task) => {
-    setTasks((prev) => prev.filter((t) => t.id !== task.id));
-    if (selectedTask?.id === task.id) {
-      setSelectedTask(null);
-      clearInput();
-    }
+    dispatch({
+      type: 'delete',
+      id: task.id
+    })
   };
 
-  const handleCancelEdition = () => {
-    setSelectedTask(null)
-    clearInput()
+  const handleCompleted = (task, value) => {
+    dispatch({
+      type: 'complete',
+      id: task.id,
+      value: value
+    })
   }
+
+
+  const isValidTask = (value) => {
+    return value !== "";
+  };
+
+  const clearInput = () => {
+    if (inputRef.current) inputRef.current.value = "";
+  };
 
   return (
     <>
       <h1>Todo List</h1>
-      <div style={{display: 'flex', flexDirection: 'column', width: "100%"}}>
-        <div style={{display: 'flex', gap: "10px"}}>
-        <Input placeholder="New Task" ref={inputRef} />
-        <Button onClick={submitTask}>
-        {selectedTask ? "Edit" : "Add"}
-      </Button>
-        {
-          selectedTask && <Button onClick={handleCancelEdition}>✖️</Button>
-        }
+      <div style={{ display: 'flex', flexDirection: 'column', width: "100%" }}>
+        <div style={{ display: 'flex', gap: "10px" }}>
+          <Input placeholder="New Task" ref={inputRef} />
+          <Button onClick={submitTask}>
+            Submit
+          </Button>
+         
         </div>
-        
-      {selectedTask && <span>You're editing task {selectedTask.name}</span>}
       </div>
-      <ul style={{width: "100%", padding: 0}}>
+      <ul style={{ width: "100%", padding: 0 }}>
         {tasks.map((task) => (
           <TaskCard
             key={task.id}
             task={task}
+            onComplete={handleCompleted}
             onDelete={handleDeleteTask}
             onEdit={handleEditTask}
           />
